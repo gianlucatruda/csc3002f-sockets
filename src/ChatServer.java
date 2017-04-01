03b42303ee937500357d49e1e5366c0a59b6a1c6
Gianluca Truda <gianlucatruda@gmail.com>
2017-04-01 10:40:07 +0200

    Split the ChatServer up into a series of functions.

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -10,56 +10,83 @@ public class ChatServer {
 
     static int PORTNUM = 1024;
 
-    public static void main(String[] args) {
 
-
+    private ServerSocket listen(int port) {
         // Creating the server socket
-        ServerSocket MyService ; try {
-            MyService = new ServerSocket(PORTNUM);
-        }
-        catch ( IOException e ) {
+        ServerSocket MyService;
+        try {
+            MyService = new ServerSocket(port);
+            System.out.println("Server socket listening...");
+            return MyService;
+        } catch (IOException e) {
             System.out.println(e);
+            return null;
         }
+    }
 
+    private Socket createSocket(ServerSocket service) {
         // server creates a client socket to communicate with the client
         Socket clientSocket = null;
         try {
-            serviceSocket = MyService.accept();
+            clientSocket = service.accept(); // Not sure about this...
+            return clientSocket;
         }
         catch ( IOException e ) {
             System.out.println(e);
-        }
-
-
-        // DataInputStream is used to receive inputs from the client
-        DataInputStream input;
-        try {
-            input = new DataInputStream(serviceSocket.getInputStream ());
-        }
-        catch ( IOException e ) {
-            System.out.println(e);
-        }
-
-        // PrintStream used to send data to client
-        PrintStream output;
-        try {
-            output = new PrintStream(serviceSocket.getOutputStream());
-        }
-        catch ( IOException e ) {
-            System.out.println(e);
-        }
-
-        // close the input and output streams and the sockets
-        try {
-            output.close ();
-            input.close ();
-            serviceSocket.close ();
-            MyService.close ();
-        }
-        catch ( IOException e ) {
-            System.out.println(e);
+            return null;
         }
 
     }
+
+    private void closeSocket(Socket clientSocket, DataInputStream input, PrintStream output) {
+        // close the input and output streams and the socket
+        try {
+            output.close ();
+            input.close ();
+            clientSocket.close ();
+        }
+        catch ( IOException e ) {
+            System.out.println(e);
+        }
+    }
+
+    private DataInputStream openInputStream(Socket clientSocket) {
+        // DataInputStream is used to receive inputs from the client
+        DataInputStream input;
+        try {
+            input = new DataInputStream(clientSocket.getInputStream());
+            return input;
+        }
+        catch ( IOException e ) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    private PrintStream openOutputStream(Socket clientSocket) {
+        // PrintStream used to send data to client
+        PrintStream output;
+        try {
+            output = new PrintStream(clientSocket.getOutputStream());
+            return output;
+        }
+        catch ( IOException e ) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+
+
+
+
+
+
 
 }
